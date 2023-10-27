b83e251808b6206393fb91d8721e71931693c30b
Vahidone <v.ban.roma@gmail.com>
2023-10-27 22:46:34 +0200

    feat: check pass has 1 uppercase, 1 lowercase... 1 number and 1 special character

--- partials/functions.php
+++ partials/functions.php
@@ -1,17 +1,28 @@
-
 <?php
 
 function generateRandomPassword($length) {
-  $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?';
-  $password = '';
-  $chars_length = strlen($chars);
+    $uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    $numbers = '0123456789';
+    $specialChars = '!@#$%^&*()_-=+;:,.?';
+    $chars = $uppercase . $lowercase . $numbers . $specialChars;
+    $password = '';
+    $chars_length = strlen($chars);
 
-  for ($i = 0; $i < $length; $i++) {
-    $password .= $chars[rand(0, $chars_length - 1)];
-  }
+    // Assicurati che la password contenga almeno una lettera minuscola, una maiuscola, un numero e un carattere speciale
+    $password .= $uppercase[rand(0, strlen($uppercase) - 1)];
+    $password .= $lowercase[rand(0, strlen($lowercase) - 1)];
+    $password .= $numbers[rand(0, strlen($numbers) - 1)];
+    $password .= $specialChars[rand(0, strlen($specialChars) - 1)];
 
-  return $password;
+    for ($i = 0; $i < $length - 4; $i++) {
+        $password .= $chars[rand(0, $chars_length - 1)];
+    }
+
+    // Mischia la password per mescolare i caratteri
+    $password = str_shuffle($password);
+
+    return $password;
 }
-
 
 ?>
